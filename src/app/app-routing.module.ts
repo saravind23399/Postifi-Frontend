f41b8ffe1d202359ba9fc3b4831bd1e863bc62fd
saravind23399 <saravind23399@gmail.com>
2019-05-11 14:29:21 +0530

    Added Routes
    Aravind S

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GoogleComponent } from './Components/Auth/google/google.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'auth/google', component: GoogleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
