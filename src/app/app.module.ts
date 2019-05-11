7c524fcb538436f10cc3b6678e0e433c1bf61001
saravind23399 <saravind23399@gmail.com>
2019-05-11 14:29:55 +0530

    Added Modules
    Aravind S

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AuthServiceConfig, GoogleLoginProvider, SocialLoginModule } from 'angularx-social-login';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleComponent } from './Components/Auth/google/google.component';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './Services/Auth/auth.service';
+import { AppService } from './Services/App/app.service';
+
+const authServiceConfig = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider(environment.auth.google.clientId)
+  },
+]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: ()=>authServiceConfig
+    },
+    AuthService,
+    AppService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
