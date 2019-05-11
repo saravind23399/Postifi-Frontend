17dca95357080e1ff43a5b52c48816a217f186ad
saravind23399 <saravind23399@gmail.com>
2019-05-11 14:27:27 +0530

    Updated App Component
    Aravind S

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Frontend';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
 }
+
