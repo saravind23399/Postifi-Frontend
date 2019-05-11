de7ccd3f03024956f75cdac8857ac5f126f0454d
saravind23399 <saravind23399@gmail.com>
2019-05-11 14:23:23 +0530

    Added Google ClientId
    Aravind S

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    google: {
+      clientId: '271364661089-186f38cugovk271ke8ktvqrmo49gd9ss.apps.googleusercontent.com'
+    }
+  },
+  backendUrl: 'http://localhost:3000',
+  frontendUrl: 'http://localhost:4200'
 };
